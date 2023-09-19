9dd2419cf0244e92858353e236d1365e5019de87
Justin Townsend <justinjtownsend@gmail.com>
2023-09-19 14:47:47 +0100

    Added some new files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 
+const compression = require('compression');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -56,13 +58,14 @@ app.use(
   })
 );
 
+app.use(compression());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Testing middleware
 app.use((req, res, next) => {
   req.reqTime = new Date().toISOString();
-  // console.log(req.headers);
 
   next();
 });
