067c9a6285fe2c22747e7be756e91815fac73152
Justin Townsend <justinjtownsend@gmail.com>
2024-01-16 11:59:30 +0000

    Added Stripe webhook checkout events

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
@@ -120,8 +121,13 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
+
+app.post(
+  'webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+); // RAW form required, not JSON!
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
